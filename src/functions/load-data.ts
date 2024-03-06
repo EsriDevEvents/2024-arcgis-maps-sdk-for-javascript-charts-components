9bf7302ae1274d423ba65b830cdad2d54e95b370
hao10315 <hchen@esri.com>
2024-03-05 21:42:52 -0800

    chore: initial commit (react template for charts)

---
+++ src/functions/load-data.ts
@@ -0,0 +1,31 @@
+/* Copyright 2024 Esri
+ *
+ * Licensed under the Apache License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+
+/**
+ * Simple data loader against public data.
+ * For the test data, we need a feature layer.
+ */
+export async function loadFeatureLayer() {
+  const featureLayer = new FeatureLayer({
+    portalItem: {
+      id: '8871626e970a4f3e9d6113ec63a92f2f',
+    },
+  });
+  await featureLayer.load();
+
+  return featureLayer;
+}
