9ffb04cfd7fb31e0ca984f92879b4ba2b190e0bc
hao10315 <hchen@esri.com>
2024-03-09 18:13:35 -0800

    chore: update first demo

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -5,164 +5,75 @@ import { ScatterPlotModel } from '@arcgis/charts-model';
 import type FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 import './Charts.css';
 
-// Define the props for the Charts component
 interface ChartsProps {
   mapElement: HTMLArcgisMapElement;
 }
 
 export default function Charts({ mapElement }: ChartsProps) {
-  // Create refs for the box plots and scatter plot
-  const boxPlotRef1 = useRef<HTMLArcgisChartsBoxPlotElement>(null);
-  const boxPlotRef2 = useRef<HTMLArcgisChartsBoxPlotElement>(null);
-  const scatterPlotRef = useRef<HTMLArcgisChartsScatterPlotElement>(null);
+  // STEP 2: Create refs for the box plots and scatterplot
+  // const boxPlotRef1 = useRef<HTMLArcgisChartsBoxPlotElement>(null);
+  // const boxPlotRef2 = useRef<HTMLArcgisChartsBoxPlotElement>(null);
 
-  // Create refs for the action bars
-  const boxPlotActionBarRef1 = useRef<HTMLArcgisChartsActionBarElement>(null);
-  const boxPlotActionBarRef2 = useRef<HTMLArcgisChartsActionBarElement>(null);
-  const scatterPlotActionBarRef = useRef<HTMLArcgisChartsActionBarElement>(null);
+  // const scatterPlotRef = useRef<HTMLArcgisChartsScatterPlotElement>(null);
 
-  // Create state for the layer view
-  const [layerView, setLayerView] = useState(null);
-
-  // Define the highlight select, set the typing to IHandle (a handle to a highlight call result, can be used to remove the highlight from the view)
-  let highlightSelect: IHandle | undefined;
-
-  // Function to save the charts
-  const saveCharts = async () => {
-    if (mapElement !== null) {
-      // Get the webmap from the map element
-      const webmap = mapElement.map;
-
-      // Get the aquiferSaturatedThicknessLayer from the webmap
-      const aquiferSaturatedThicknessLayer = webmap.findLayerById('18dfc8cf7b7-layer-16') as FeatureLayer;
-
-      // Get the scatter plot config
-      const scatterPlotConfig = scatterPlotRef.current.config;
-
-      // Push the scatter plot config to the charts of the aquiferSaturatedThicknessLayer
-      aquiferSaturatedThicknessLayer.charts.push(scatterPlotConfig);
-
-      // Save the webmap
-      await webmap.save();
-    }
-  };
-
-  // Function to initialize the chart
   const initializeChart = useCallback(async () => {
     if (mapElement !== null) {
-      // Get the webmap and view from the map element
-      const webmap = mapElement.map;
-      const view = mapElement.view;
-
-      // Get the aquiferSaturatedThicknessLayer and waterDepthPercentageChangeLayer from the webmap
-      const aquiferSaturatedThicknessLayer = webmap.findLayerById('18dfc8cf7b7-layer-16') as FeatureLayer;
-      const waterDepthPercentageChangeLayer = webmap.findLayerById('18df37f7e52-layer-67') as FeatureLayer;
-
-      // Get the layer view for the waterDepthPercentageChangeLayer
-      const layerView = await view.whenLayerView(waterDepthPercentageChangeLayer);
-
-      // Set the layer view
-      setLayerView(layerView);
-
-      // Get the box plot configs from the waterDepthPercentageChangeLayer
-      const boxPlotConfig1 = waterDepthPercentageChangeLayer.charts[0];
-      const boxPlotConfig2 = waterDepthPercentageChangeLayer.charts[1];
-
-      // Set the configs and layer for the box plots
-      boxPlotRef1.current.config = boxPlotConfig1;
-      boxPlotRef1.current.layer = waterDepthPercentageChangeLayer;
-
-      boxPlotRef2.current.config = boxPlotConfig2;
-      boxPlotRef2.current.layer = waterDepthPercentageChangeLayer;
-
-      // Set the default actions for the box plot action bar
-      setDefaultActionBar(boxPlotActionBarRef1.current, 'boxPlotSeries');
-      setDefaultActionBar(boxPlotActionBarRef2.current, 'boxPlotSeries');
-
-      // ================================================ Scatterplot (with Model) ===================================================
-      // Define the scatter plot params
-      const scatterPlotParams = {
-        layer: aquiferSaturatedThicknessLayer,
-        xAxisFieldName: 'YEAR1974',
-        yAxisFieldName: 'YEAR2014',
-      };
-
-      // Create a new scatter plot model with the scatter plot params
-      const scatterPlotModel = new ScatterPlotModel(scatterPlotParams);
-
-      // Get the config from the scatter plot model
-      const config = await scatterPlotModel.config;
-
-      // Set the config and layer for the scatter plot
-      scatterPlotRef.current.config = config;
-      scatterPlotRef.current.layer = aquiferSaturatedThicknessLayer;
-
-      // Set the default actions for the scatter plot action bar
-      setDefaultActionBar(scatterPlotActionBarRef.current, 'scatterSeries');
+      // STEP 3: Get the map object from the mapElemet prop being passed in from map-component event listener
+      // const map = mapElement.map;
+      //
+      // STEP 4: Get the aquiferSaturatedThicknessLayer and waterDepthPercentageChangeLayer from the mao by the unique layerId
+      // const waterDepthPercentageChangeLayer = map.findLayerById('18df37f7e52-layer-67') as FeatureLayer;
+      //
+      // STEP 5: Get the box plot configs from the waterDepthPercentageChangeLayer
+      // const boxPlotConfig1 = waterDepthPercentageChangeLayer.charts[0];
+      // const boxPlotConfig2 = waterDepthPercentageChangeLayer.charts[1];
+      //
+      // STEP 6: Set the config and layer for the box plots
+      // boxPlotRef1.current.config = boxPlotConfig1;
+      // boxPlotRef1.current.layer = waterDepthPercentageChangeLayer;
+      // boxPlotRef2.current.config = boxPlotConfig2;
+      // boxPlotRef2.current.layer = waterDepthPercentageChangeLayer;
+      // ======================== Scatterplot (with Model) ===================================================
+      // STEP 1: Create scatter plot params
+      // const aquiferSaturatedThicknessLayer = map.findLayerById('18dfc8cf7b7-layer-16') as FeatureLayer;
+      // const scatterPlotParams = {
+      //   layer: aquiferSaturatedThicknessLayer,
+      //   xAxisFieldName: 'YEAR1974',
+      //   yAxisFieldName: 'YEAR2014',
+      // };
+      //
+      // STEP 2: Create a new scatter plot model with the scatter plot params
+      // const scatterPlotModel = new ScatterPlotModel(scatterPlotParams);
+      //
+      // STEP 3: Get the config from the scatter plot model
+      // const config = await scatterPlotModel.config;
+      //
+      // STEP 4: Set the config and layer for the scatter plot
+      // scatterPlotRef.current.config = config;
+      // scatterPlotRef.current.layer = aquiferSaturatedThicknessLayer;
     }
   }, [mapElement]);
-
-  // Function to handle the selection complete event
-  const handleArcgisChartsSelectionComplete = (actionBarRef: React.RefObject<HTMLArcgisChartsActionBarElement>) => (event: CustomEvent) => {
-    // Get the selection data from the event detail which is of type HTMLArcgisChartsBarChartElement["selectionData"]
-    const selectionOIDs = event.detail.selectionOIDs;
-
-    // Remove the previous highlight select and set the new one
-    highlightSelect?.remove();
-    highlightSelect = layerView.highlight(selectionOIDs);
-
-    // Enable or disable the clear selection and filter by selection buttons based on the selection data
-    if (selectionOIDs === undefined || selectionOIDs.length === 0) {
-      actionBarRef.current.disableClearSelection = true;
-      actionBarRef.current.disableFilterBySelection = true;
-    } else {
-      actionBarRef.current.disableClearSelection = false;
-      actionBarRef.current.disableFilterBySelection = false;
-    }
-  };
 
   // Use effect to initialize the chart
   useEffect(() => {
     initializeChart().catch(console.error);
   }, [initializeChart]);
 
-  // Render the component
+  // STEP 1: Add the ArcgisChartsBoxPlot and ArcgisChartsScatterPlot components to the CalciteBlocks
   return (
     <div data-panel-id='charts'>
-      <CalciteButton kind='inverse' icon-start='save' class='calcite-mode-dark' onClick={saveCharts}>
-        Save Charts
-      </CalciteButton>
       <CalciteBlock class='chart-block' collapsible heading='Distribution of Water Measurement Data since 1974'>
-        <ArcgisChartsBoxPlot ref={boxPlotRef1} onArcgisChartsSelectionComplete={handleArcgisChartsSelectionComplete(boxPlotActionBarRef1)}>
-          <ArcgisChartsActionBar slot='action-bar' ref={boxPlotActionBarRef1}></ArcgisChartsActionBar>
-        </ArcgisChartsBoxPlot>
+        {/* <ArcgisChartsBoxPlot></ArcgisChartsBoxPlot> */}
         <CalciteIcon scale='s' slot='icon' icon='box-chart'></CalciteIcon>
       </CalciteBlock>
       <CalciteBlock class='chart-block' collapsible heading='Distribution of Water Measurement Data in 2024'>
-        <ArcgisChartsBoxPlot ref={boxPlotRef2} onArcgisChartsSelectionComplete={handleArcgisChartsSelectionComplete(boxPlotActionBarRef2)}>
-          <ArcgisChartsActionBar slot='action-bar' ref={boxPlotActionBarRef2}></ArcgisChartsActionBar>
-        </ArcgisChartsBoxPlot>
+        {/* <ArcgisChartsBoxPlot></ArcgisChartsBoxPlot> */}
         <CalciteIcon scale='s' slot='icon' icon='box-chart'></CalciteIcon>
       </CalciteBlock>
       <CalciteBlock class='chart-block' collapsible heading='Depth of Water (1974 vs 2024) sized by Saturated Thickness'>
-        <ArcgisChartsScatterPlot ref={scatterPlotRef} onArcgisChartsSelectionComplete={handleArcgisChartsSelectionComplete(scatterPlotActionBarRef)}>
-          <ArcgisChartsActionBar slot='action-bar' ref={scatterPlotActionBarRef}></ArcgisChartsActionBar>
-        </ArcgisChartsScatterPlot>
+        {/* <ArcgisChartsScatterPlot></ArcgisChartsScatterPlot> */}
         <CalciteIcon scale='s' slot='icon' icon='graph-scatter-plot'></CalciteIcon>
       </CalciteBlock>
     </div>
   );
-}
-
-// Function to set the default actions for an action bar
-function setDefaultActionBar(actionBarElement: HTMLArcgisChartsActionBarElement, seriesType: string) {
-  if (actionBarElement !== null) {
-    actionBarElement.actionBarHideActionsProps = {
-      hideRotateChart: seriesType === 'histogramSeries' || seriesType === 'pieSeries' || seriesType === 'scatterSeries',
-      hideFilterByExtent: true,
-      hideZoom: true,
-      hideSelection: true,
-      hideFullExtent: true,
-    };
-  }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,27 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import Charts from './components/Charts';
-import {
-  CalciteShellPanel,
-  CalcitePanel,
-  CalciteShell,
-  CalciteNavigation,
-  CalciteNavigationLogo
-} from '@esri/calcite-components-react';
+import { CalciteShellPanel, CalcitePanel, CalciteShell, CalciteNavigation, CalciteNavigationLogo } from '@esri/calcite-components-react';
 import { ArcgisMap, ArcgisHome } from '@arcgis/map-components-react';
-
-import { defineCustomElements as defineCalciteElements } from '@esri/calcite-components/dist/loader';
-import { defineCustomElements as defineChartsElements } from '@arcgis/charts-components/dist/loader';
-import { defineCustomElements as defineMapElements } from '@arcgis/map-components/dist/loader';
 
 import './style.css';
 
-// define custom elements in the browser, and load the assets from the CDN
-defineChartsElements(window, { resourcesUrl: 'https://js.arcgis.com/charts-components/4.29/t9n' });
+import { defineCustomElements as defineCalciteElements } from '@esri/calcite-components/dist/loader';
+import { defineCustomElements as defineMapElements } from '@arcgis/map-components/dist/loader';
+
 defineCalciteElements(window, { resourcesUrl: 'https://js.arcgis.com/calcite-components/2.4.0/assets' });
 defineMapElements(window, { resourcesUrl: 'https://js.arcgis.com/map-components/4.29/assets' });
+
+// STEP 1: Import the defineCustomElements function from the charts-components package
+// import { defineCustomElements as defineChartsElements } from '@arcgis/charts-components/dist/loader';
+
+// STEP 2: Define the charts elements in the browser, and load the necessary assets from the CDN
+// defineChartsElements(window, { resourcesUrl: 'https://js.arcgis.com/charts-components/4.29/t9n' });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function App() {
-  const [element, setElement] = useState<HTMLArcgisMapElement>(null);
+  const [mapElement, setMapElement] = useState<HTMLArcgisMapElement>(null);
 
   return (
     <React.StrictMode>
@@ -33,8 +30,8 @@ function App() {
           <ArcgisMap
             id='mapEl'
             item-id='57919ac3b4a04a00b7d150d679eb866a'
-            onArcgisViewReadyChange={(e) => {
-              setElement(e.target);
+            onArcgisViewReadyChange={(event) => {
+              setMapElement(event.target);
             }}
           >
             <ArcgisHome position='top-right'></ArcgisHome>
@@ -51,7 +48,8 @@ function App() {
           </CalciteNavigation>
 
           <CalcitePanel>
-            <Charts mapElement={element} />
+            {/* STEP 3: Include the custom Charts react element with the mapElement prop */}
+            {/* <Charts mapElement={mapElement} /> */}
           </CalcitePanel>
         </CalciteShellPanel>
       </CalciteShell>
